588b1c033e33de8e141852e87dd5ac5057f137a7
Ahmed Ansari <ahmedalam97@gmail.com>
2022-05-06 20:43:39 +0500

    removed syntax errors

--- as5048b.cpp
+++ as5048b.cpp
@@ -1,51 +1,55 @@
-#include "as5048b.h"
+#include "AS5048B.h"
 
 AS5048B::AS5048B(uint8_t addr = AS5048_ADDRESS) {
+  // Constructor sets address of the encoder. Default address is 0x40
   chipAddress = addr;
   AS5048B::I2CSetup();
 }
 
-AS5048B::I2CSetup() {
+void AS5048B::I2CSetup() {
+  // I2C setup
   file = wiringPiI2CSetup(chipAddress);
 
   if (file == -1) {
-    printf("Failed to init I2C communication.\n") usleep(1000);
+    printf("Failed to init I2C communication.\n");
+    usleep(1000);
     exit(0);
   }
-  
-  printf("I2C communication successfully setup.\n");
 
+  printf("I2C communication successfully setup.\n");
   usleep(1000);
 }
 
 double AS5048B::angle(int unit, bool newVal) {
-  value_16bit = wiringPiI2CReadReg16(file, AS5048B_ANGLMSB_REG);
+  // Function that outputs the encoder angle in the desired unit
+  value_16bit = wiringPiI2CReadReg16(
+      file, AS5048B_ANGLMSB_REG);  // Read the MSB and the LSB register
 
-  // in case LSB:MSB
+  // most significant bit
   value_MSB =
       value_16bit &
       0xFF;  // MSB is already in position, just cut of the unwanted part
+
   value_LSB = value_16bit >> 8;  // shift LSB to the right position
 
-  // then the normal bitbanging to get the 14bit value (according the datasheet
-  // of the sensor)
   value_14bit = ((uint16_t)value_MSB)
                 << 6;  // cast the 8 bit value to 16bit size and shift the MSB
-                       // value 6 position to the left
+
+  // value 6 position to the left
+
   value_14bit +=
       (value_LSB &
        0x3f);  // mask/remove the top two bits of LSB and add the other to the
-               // previous result to get the final result
 
+  // previous result to get the final result
   raw_ang = (double)value_14bit;  // cast the unsigned 16 bit value to double
-                                  // floating point value
 
+  // floating point value
   return AS5048B::convertAngle(unit, raw_ang);
 }
 
 double AS5048B::convertAngle(int unit, double angle) {
   // convert raw sensor reading into angle unit
-
   double angleConv;
 
   switch (unit) {
@@ -53,54 +57,66 @@ double AS5048B::convertAngle(int unit, double angle) {
       // Sensor raw measurement
       angleConv = angle;
       break;
+
     case U_TRN:
       // full turn ratio
       angleConv = (angle / AS5048B_RESOLUTION);
       break;
+
     case U_DEG:
       // degree
       angleConv = (angle / AS5048B_RESOLUTION) * 360.0;
       break;
+
     case U_RAD:
       // Radian
       angleConv = (angle / AS5048B_RESOLUTION) * 2 * 3.14;
       break;
+
     case U_MOA:
       // minute of arc
       angleConv = (angle / AS5048B_RESOLUTION) * 60.0 * 360.0;
       break;
+
     case U_SOA:
       // second of arc
       angleConv = (angle / AS5048B_RESOLUTION) * 60.0 * 60.0 * 360.0;
       break;
+
     case U_GRAD:
       // grade
       angleConv = (angle / AS5048B_RESOLUTION) * 400.0;
       break;
+
     case U_MILNATO:
       // NATO MIL
       angleConv = (angle / AS5048B_RESOLUTION) * 6400.0;
       break;
+
     case U_MILSE:
       // Swedish MIL
       angleConv = (angle / AS5048B_RESOLUTION) * 6300.0;
       break;
+
     case U_MILRU:
       // Russian MIL
       angleConv = (angle / AS5048B_RESOLUTION) * 6000.0;
       break;
+
     default:
       // no conversion => raw angle
       angleConv = angle;
       break;
   }
+
   return angleConv;
 }
 
 void AS5048B::doProgZero(void) {
-  // this will burn the zero position OTP register as described in the
-  // datasheet enable programming mode
+  // this will burn the zero position OTP register as described in the datasheet 
+  // enable programming mode
   AS5048B::progRegister(0x01);
+
   usleep(10000);
 
   // set the burn bit: enables automatic programming procedure
@@ -124,8 +140,18 @@ void AS5048B::progRegister(uint8_t regVal) {
   wiringPiI2CWriteReg8(file, AS5048B_PROG_REG, regVal);
 }
 
+void AS5048B::setZeroReg(void) {
+  AS5048B::zeroRegW((uint16_t)0x00);
+  uint16_t newZero = AS5048B::angle(U_RAW, true);
+  AS5048B::zeroRegW(newZero);
+}
+
+void AS5048B::zeroRegW(uint16_t regVal) {
+  wiringPiI2CWriteReg8(file, AS5048B_ZEROMSB_REG, (uint8_t)(regVal >> 6));
+  wiringPiI2CWriteReg8(file, AS5048B_ZEROLSB_REG, (uint8_t)(regVal & 0x3F));
+}
+
 void AS5048B::burnZero(void) {
-  AS5048B::setZeroReg();  // Set the zero registory
-  usleep(1000);           // Wait for 1000 us
-  AS5048B::doProgZero();  // Burn the zero angle
+  AS5048B::setZeroReg();
+  AS5048B::doProgZero();
 }
--- as5048b.h
+++ as5048b.h
@@ -1,24 +1,32 @@
 #ifndef AS5048B_H
 #define AS5048B_H
 
-#include <wiringPiI2c.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <wiringPiI2C.h>
+
+#include <cstdlib>
+
+using namespace std;
 
 // Default addresses for AS5048B
-#define AS5048_ADDRESS 0x40 // 0b10000 + ( A1 & A2 to GND)
+
+#define AS5048_ADDRESS 0x40  // 0b10000 + ( A1 & A2 to GND)
 #define AS5048B_PROG_REG 0x03
 #define AS5048B_ADDR_REG 0x15
-#define AS5048B_ZEROMSB_REG 0x16 //bits 0..7
-#define AS5048B_ZEROLSB_REG 0x17 //bits 0..5
+#define AS5048B_ZEROMSB_REG 0x16  // bits 0..7
+#define AS5048B_ZEROLSB_REG 0x17  // bits 0..5
 #define AS5048B_GAIN_REG 0xFA
 #define AS5048B_DIAG_REG 0xFB
-#define AS5048B_MAGNMSB_REG 0xFC //bits 0..7
-#define AS5048B_MAGNLSB_REG 0xFD //bits 0..5
-#define AS5048B_ANGLMSB_REG 0xFE //bits 0..7
-#define AS5048B_ANGLLSB_REG 0xFF //bits 0..5
-#define AS5048B_RESOLUTION 16384.0 //14 bits
+#define AS5048B_MAGNMSB_REG 0xFC    // bits 0..7
+#define AS5048B_MAGNLSB_REG 0xFD    // bits 0..5
+#define AS5048B_ANGLMSB_REG 0xFE    // bits 0..7
+#define AS5048B_ANGLLSB_REG 0xFF    // bits 0..5
+#define AS5048B_RESOLUTION 16384.0  // 14 bits
 
-//unit consts - just to make the units more readable
+// unit consts - just to make the units more readable
+
 #define U_RAW 1
 #define U_TRN 2
 #define U_DEG 3
@@ -30,57 +38,52 @@
 #define U_MILSE 9
 #define U_MILRU 10
 
-class AS5048B{
+class AS5048B {
+ private:
+  int file;
+  int adapter_nr;
+  char filename[20];
+  int addr;
+  char buf[10];
 
-private:
-    int file;
-    int adapter_nr;
-    char filename[20];
-    int addr;
-    char buf[10];
+  // variables
+  bool _debugFlag;
+  uint8_t chipAddress;
+  uint8_t _addressRegVal;
+  uint16_t _zeroRegVal;
+  double _lastAngleRaw;
+  double _movingAvgExpAngle;
+  double _movingAvgExpSin;
+  double _movingAvgExpCos;
+  double _movingAvgExpAlpha;
+  int _movingAvgCountLoop;
+  uint8_t value_MSB;
+  uint8_t value_LSB;
+  uint16_t value_16bit;
+  uint16_t value_14bit;
+  double raw_ang;
 
-    //variables
-	bool	    _debugFlag;
-	uint8_t		chipAddress;
-	uint8_t		_addressRegVal;
-	uint16_t	_zeroRegVal;
-	double		_lastAngleRaw;
-	double		_movingAvgExpAngle;
-	double		_movingAvgExpSin;
-	double		_movingAvgExpCos;
-	double		_movingAvgExpAlpha;
-	int		_movingAvgCountLoop;
+ public:
+  AS5048B(uint8_t);
 
-	uint8_t		value_MSB;
-	uint8_t		value_LSB;
-	uint16_t	value_16bit;
-	uint16_t	value_14bit;
-	double		raw_ang;
+  // methods
+  void I2CSetup(void);
+  double convertAngle(
+      int,
+      double);  // RAW, TRN, DEG, RAD, GRAD, MOA, SOA, MILNATO, MILSE, MILRU
 
-	
-
-public:
-    AS5048B(uint8_t);
-
-    //methods
-    void        I2CSetup();
-        
-	double		convertAngle(int, double); //RAW, TRN, DEG, RAD, GRAD, MOA, SOA, MILNATO, MILSE, MILRU
-	double		getExpAvgRawAngle(void);
-
-	void		progRegister(uint8_t regVal); //nothing so far - manipulate the OTP register
-	void		doProg(void); //progress programming slave address OTP
-	void		doProgZero(void); //progress programming zero position OTP
-	void		setZeroReg(void); //set Zero to current angle position
-	void		zeroRegW(uint16_t regVal); //write Zero register value
-	uint16_t	zeroRegR(void); //read Zero register value
-	void		burnZero(void);
-
-	uint16_t	angleRegR(void); //read raw value of the angle register
-    double		angle(int unit = U_RAW, bool newVal = true)
-    uint8_t		diagR(void); //read diagnostic register
-
+  double getExpAvgRawAngle(void);
+  void progRegister(
+      uint8_t regVal);    // nothing so far - manipulate the OTP register
+  void doProg(void);      // progress programming slave address OTP
+  void doProgZero(void);  // progress programming zero position OTP
+  void setZeroReg(void);  // set Zero to current angle position
+  void zeroRegW(uint16_t regVal);  // write Zero register value
+  uint16_t zeroRegR(void);         // read Zero register value
+  void burnZero(void);
+  uint16_t angleRegR(void);  // read raw value of the angle register
+  double angle(int unit = U_RAW, bool newVal = true);
+  uint8_t diagR(void);  // read diagnostic register
 };
-
 
 #endif
